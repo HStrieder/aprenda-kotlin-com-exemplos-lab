2786aeb0eadd6b314110e1194ac868f23082e58d
Henrique Strieder <105461637+HStrieder@users.noreply.github.com>
2023-10-27 00:11:55 -0300

    desafio feito

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,35 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("Usuário ${usuario.nome} matriculado na formação $nome.")
+    }
+
+    fun listarInscritos() {
+        println("Inscritos na formação $nome:")
+        inscritos.forEach { println(it.nome) }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("Bob")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 45)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 60)
+
+    val formacao = Formacao("Desenvolvimento de Software", listOf(conteudo1, conteudo2))
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    formacao.listarInscritos()
 }
